ad54326a0a56011b1fc3603b5453a48bd2a304a2
dhiena2702 <23203007@srcas.ac.in>
2024-06-22 12:00:16 +0530

    Create dhiena.java

---
+++ dhiena.java
@@ -0,0 +1,9 @@
+public class Hello{ 
+public static void main(Strings[]args){ 
+system.out.println("welcome"); 
+system.out.println("to"); 
+system.out.println("Devops"); 
+system.out.println("class"); 
+} 
+}
+
